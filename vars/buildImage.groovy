5e6628e38c6cda3660e9e393cb8502aca127a212
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-12-31 10:46:18 +1300

    docker build with param and use global environment variable from job

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh'docker build -t <repo domain>/<image name>:<tag> .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-        sh 'docker push <repo domain>/<image name>:<tag>'
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the application..."
+    // Referencing Jenkins job global env variable
+    echo "building the application for branch $BRANCH_NAME..."
     sh "mvn package"
 }
